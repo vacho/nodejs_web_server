7313c439fc6e6053ca117065becf1e04ad10cf2c
vacho <vachisinio@gmail.com>
2025-12-20 16:17:45 -0400

    Adding routes management in next

--- server.js
+++ server.js
@@ -3,8 +3,40 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-app.get('/', (req, res) => {
-    res.send('Hello Osval World!');
+app.get(/^\/($|index(\.html)?)/, (req, res) => {
+    //res.sendFile('./views/index.html', { root: __dirname });
+    res.sendFile(path.join(__dirname, 'views', 'index.html')); 
+});
+app.get(/new-page(\.html)?/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html')); 
+});
+app.get(/old-page(\.html)?/, (req, res) => {
+    res.redirect(301, '/new-page.html'); // 302 by default.
+});
+app.get(/hello(\.html)?/, (req, res, next) => {
+    console.log('Attepted to load hello.html')
+    next();
+}, (req, res) => {
+    res.send('Hello World!');
+});
+
+// Chaining route handlers.
+const one = (req, res, next) => {
+    console.log('One');
+    next();
+};
+const two = (req, res, next) => {
+    console.log('Two');
+    next();
+};
+const three = (req, res) => {
+    console.log('Three');
+    res.send('Finished!');
+};
+app.get('/chain', [one, two, three]);
+
+app.get(/(.*)/, (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
