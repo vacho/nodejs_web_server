122e57aea394c942a758fd11306053b0ad7cf269
vacho <vachisinio@gmail.com>
2025-12-22 10:22:35 -0400

    Add middleware to error log

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
 
 // Middlewares
@@ -11,6 +13,20 @@ app.use(express.static(path.join(__dirname, 'public'))); // Serve static files f
 
 // Custom Middleware for Logging
 app.use(logger);
+
+// Cross-Origin Resource Sharing
+const whitelist = ['https://www.google.com', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.get(/^\/($|index(\.html)?)/, (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
@@ -45,7 +61,16 @@ const three = (req, res) => {
 app.get('/chain', [one, two, three]);
 
 app.get(/(.*)/, (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
